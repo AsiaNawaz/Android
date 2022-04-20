efd1bc7457a04478a4eee621b482128cb4fd980e
Asia <190768@students.au.edu.pk>
2022-04-21 04:43:54 +0500

    Updates

---
+++ precache-manifest.b6dfed3d30957b2fe2595ab4b54d414e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dd046cd94fc8c59607c",
+    "url": "/Android-App/static/css/main.40695f88.chunk.css"
+  },
+  {
+    "revision": "9dd046cd94fc8c59607c",
+    "url": "/Android-App/static/js/main.9dd046cd.chunk.js"
+  },
+  {
+    "revision": "8350b9eb958d55b7e207",
+    "url": "/Android-App/static/js/1.8350b9eb.chunk.js"
+  },
+  {
+    "revision": "5015334de6b5b90cb313",
+    "url": "/Android-App/static/js/2.5015334d.chunk.js"
+  },
+  {
+    "revision": "6b4aa82d08df1541cb69",
+    "url": "/Android-App/static/js/runtime~main.6b4aa82d.js"
+  },
+  {
+    "revision": "a57ede0e2aabfbd5544caf537c9a87d2",
+    "url": "/Android-App/static/media/Andriller tool.a57ede0e.gif"
+  },
+  {
+    "revision": "2f5ddb8b3f89c973e88b850a7fae9299",
+    "url": "/Android-App/static/media/ufed.2f5ddb8b.gif"
+  },
+  {
+    "revision": "8844bef122d80144c0623e7ca50e1528",
+    "url": "/Android-App/static/media/MEFT.8844bef1.gif"
+  },
+  {
+    "revision": "64dd04924af4aa2d87937296f125979d",
+    "url": "/Android-App/static/media/as.64dd0492.gif"
+  },
+  {
+    "revision": "69855b2300715e302a86a4a6b5b6accd",
+    "url": "/Android-App/static/media/img9.69855b23.jpg"
+  },
+  {
+    "revision": "b52948b85e243aec93c0e118fe621419",
+    "url": "/Android-App/static/media/df.b52948b8.png"
+  },
+  {
+    "revision": "c70b4b1cdb792bbc36a6931c9421d34d",
+    "url": "/Android-App/index.html"
+  }
+];
